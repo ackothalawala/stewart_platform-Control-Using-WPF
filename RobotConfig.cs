fafb7e94cd9fdb6b15fffa21826c65af831dfa00
Avishka Kothalawala <90367148+ackothalawala@users.noreply.github.com>
2026-02-12 08:56:22 +0530

    updated maths to zero servo positions

--- RobotConfig.cs
+++ RobotConfig.cs
@@ -9,33 +9,29 @@ namespace stewart_platform
         public double PlatformRadius { get; set; } = 56.0;
         public double HornLength { get; set; } = 36.845;
         public double RodLength { get; set; } = 144.0;
-        public double InitialHeight { get; set; } = 135.0;
+        public double InitialHeight { get; set; } = 132.890;
 
         // --- Base & Platform Angles (degrees) ---
         public double[] BaseAngles { get; set; } =
-            { -60, -120, -180, -240, -300, -360 };
+            { -50, -70, -170, -190, -290, -310 };
 
         public double[] PlatformAngles { get; set; } =
-            { -75, -105, -195, -225, -315,-345 };
+            {-80, -100, -200, -220, -320, -340 };
 
         // --- Servo Orientation (Beta) in radians ---
         public double[] BetaAngles { get; set; } =
         {
-           Math.PI / 6,      // Servo 0: -60° + 90° = 30° (PI/6)
-             5 * Math.PI / 6,  // Servo 1: -120° - 90° = -210° -> 150° (5PI/6)
-             -Math.PI / 2,     // Servo 2: -180° + 90° = -90° (-PI/2)
-             Math.PI / 6,      // Servo 3: -240° - 90° = -330° -> 30° (PI/6)
-             5 * Math.PI / 6,  // Servo 4: -300° + 90° = -210° -> 150° (5PI/6)
-             -Math.PI / 2
+           Math.PI / 6, -5 * Math.PI / 6, -Math.PI / 2,
+           Math.PI / 2, 5 * Math.PI / 6, -Math.PI / 6
         };
 
         // --- HOME ORIENTATION OFFSET ---
         // Compensates geometric yaw caused by optimized platform layout
-        public double HomeYawOffsetDeg { get; set; } = 0.0;
+        public double HomeYawOffsetDeg { get; set; } = 30.0;
 
         // --- Motion Limits ---
-        public double MaxTranslation { get; set; } = 35.0;
-        public double MaxRotation { get; set; } = 5.0;
+        public double MaxTranslation { get; set; } = 30.0;
+        public double MaxRotation { get; set; } = 30.0;
 
         // --- Serial ---
         public int BaudRate { get; set; } = 115200;
